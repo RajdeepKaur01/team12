dceae48503b295a472b1a40337ee458ffc9b7444
Mohit Gupta <gupta.moh@husky.neu.edu>
2017-02-19 14:41:53 -0500

    added note-entity

---
+++ group12/src/main/java/entities/Note.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class Note {
+	
+	protected int key;
+	protected String unicode;
+	protected String affiliation;
+	public int getKey() {
+		return key;
+	}
+	public void setKey(int key) {
+		this.key = key;
+	}
+	public String getUnicode() {
+		return unicode;
+	}
+	public void setUnicode(String unicode) {
+		this.unicode = unicode;
+	}
+	public String getAffiliation() {
+		return affiliation;
+	}
+	public void setAffiliation(String affiliation) {
+		this.affiliation = affiliation;
+	}
+
+}
