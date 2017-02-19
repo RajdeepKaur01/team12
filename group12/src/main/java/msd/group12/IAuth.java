d2b7f171072360702b1df9cad86ed51ea4f99d48
sudeepk <sudeepk@ccs.neu.edu>
2017-02-19 14:19:20 -0500

    Minor fix

--- group12/src/main/java/msd/group12/IAuth.java
+++ group12/src/main/java/msd/group12/IAuth.java
@@ -2,8 +2,8 @@ package msd.group12;
 
 public interface IAuth {
 
-	public User register(Person person, String username, String password);
-	public boolean login(String username, String password);
-	public boolean resetPassword(String username,String newPassword, String oldPassword);
-	public boolean retrieveUsername(String emailAddress);
+	public User register (Person person, String username, String password);
+	public boolean login (String username, String password);
+	public boolean resetPassword (String username, String newPassword, String oldPassword);
+	public String retrieveUsername (String emailAddress);
 }
