9a63da7e322551bc930aa43936452e23fedf425a
rajdeep <rajdeep@Rajdeeps-MacBook-Air.local>
2017-02-19 16:29:29 -0500

    phase2Deliverables

---
+++ phase2/entities/Author.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package entities;
+
+import java.net.URL;
+import java.util.List;
+
+/**
+ * This class represents an Author of research paper(s)
+ * in the database
+ */
+public class Author extends Person{
+	
+	private String[] aliases;
+	private URL homePageURL;
+	private int yearsAsCommitteeMemember;
+	private String areaOfExpertise;
+	//The list of research papers - both journal articles and conference articles
+	private List<ResearchPaper> researchPapers;
+	
+	public String[] getAliases() {
+		return aliases;
+	}
+	public void setAliases(String[] aliases) {
+		this.aliases = aliases;
+	}
+	public URL getHomePageURL() {
+		return homePageURL;
+	}
+	public void setHomePageURL(URL homePageURL) {
+		this.homePageURL = homePageURL;
+	}
+	public int getyearsAsCommitteeMemember() {
+		return yearsAsCommitteeMemember;
+	}
+	public void setyearsAsCommitteeMemember(int yearsAsCommitteeMemember) {
+		this.yearsAsCommitteeMemember = yearsAsCommitteeMemember;
+	}
+	public String getAreaOfExpertise() {
+		return areaOfExpertise;
+	}
+	public void setAreaOfExpertise(String areaOfExpertise) {
+		this.areaOfExpertise = areaOfExpertise;
+	}
+	public List<ResearchPaper> getResearchPapers() {
+		return researchPapers;
+	}
+	public void setResearchPapers(List<ResearchPaper> researchPapers) {
+		this.researchPapers = researchPapers;
+	}
+}
---
+++ phase2/entities/Inproceeding.java
@@ -0,0 +1,26 @@
+package entities;
+
+/**
+ * This represents a research paper that was submitted 
+ * in a proceedings of research conference
+ *
+ */
+public class Inproceeding extends ResearchPaper{
+	private String bookTitle;
+	
+	//The proceedings in which the research paper was published
+	private Proceedings proceedings;
+	
+	public String getBookTitle() {
+		return bookTitle;
+	}
+	public void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
+	}
+	public Proceedings getProceedings() {
+		return proceedings;
+	}
+	public void setProceedings(Proceedings proceedings) {
+		this.proceedings = proceedings;
+	}
+}
