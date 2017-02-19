05f22d18d1716b621262dd3d1263e0242356c7f1
Ira Tiwari <ira025@ccs.neu.edu ; tiwari.i@husky.neu.edu>
2017-02-19 14:06:52 -0500

    minor changes to test commit

--- group12/src/main/java/msd/group12/IFilterResults.java
+++ group12/src/main/java/msd/group12/IFilterResults.java
@@ -5,9 +5,13 @@ import java.util.List;
 public interface IFilterResults {
 
 	public List<Author> filterByLocation (String address, List<Author> listOfAuthors);
+	
 	public List<Author> filterByAge (int age, List<Author> listOfAuthors);
+	
 	public List<Author> filterByGender (String gender, List<Author> listOfAuthors);
+	
 	public List<Author> filterByYearOnCommittee (int yearsAsCommitteeMem, List<Author> listOfAuthors);
+	
 	public List<Author> filterByAreaOfExpertise (String areaOfExpertise , List<Author> listOfAuthors);
 
 }
