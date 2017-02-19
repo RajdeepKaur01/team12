593c718f25d007b2828bae1e047f50ed6fd7552e
Ira Tiwari <ira025@ccs.neu.edu ; tiwari.i@husky.neu.edu>
2017-02-19 14:55:42 -0500

    Inproceedings class

---
+++ group12/src/main/java/entities/Inproceedings.java
@@ -0,0 +1,15 @@
+package entities;
+
+public class Inproceedings extends Proceedings{
+	private String bookTitle;
+
+	public String getBookTitle() {
+		return bookTitle;
+	}
+
+	public void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
+	}
+	
+	
+}
