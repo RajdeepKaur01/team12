889d68be566006e12cc8dd9bd4b64c659e4d99e6
sudeepk <sudeepk@ccs.neu.edu>
2017-02-19 14:11:30 -0500

    Minor fixes

--- group12/src/main/java/msd/group12/IFilterResults.java
+++ group12/src/main/java/msd/group12/IFilterResults.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 public interface IFilterResults {
 
-	public List<Author> filterByLocation (String address, List<Author> listOfAuthors);
-	public List<Author> filterByAge (int age, List<Author> listOfAuthors);
-	public List<Author> filterByGender (String gender, List<Author> listOfAuthors);
-	public List<Author> filterByYearOnCommittee (int yearsAsCommitteeMem, List<Author> listOfAuthors);
-	public List<Author> filterByAreaOfExpertise (String areaOfExpertise , List<Author> listOfAuthors);
+	public List<Author> filterByLocation (String address, List<Author> authors);
+	
+	public List<Author> filterByAge (int age, List<Author> authors);
+	
+	public List<Author> filterByGender (String gender, List<Author> authors);
+	
+	public List<Author> filterByYearsOnCommittee (int yearsAsCommitteeMem, List<Author> authors);
+	
+	public List<Author> filterByAreaOfExpertise (String areaOfExpertise , List<Author> authors);
 
 }
