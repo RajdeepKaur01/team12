31d92e639dbe148659fc011cbe9955295d02cf03
sudeepk <sudeepk@ccs.neu.edu>
2017-03-25 14:02:17 -0400

    updated

--- src/main/java/queryengine/DAO.java
+++ src/main/java/queryengine/DAO.java
@@ -1,7 +1,12 @@
 package main.java.queryengine;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
 public interface DAO <T>{
-	public T findById (String id);
-	public T findByAttribute (String attributeName, String attributeValue);
+	public T findById (int id) throws SQLException;
+	public List<T> findByAttributes (Map<String, String> attributeNamesAndValues);
+	public List<T> findByAttribute (String attirubteName, String attributeValue);
 	public boolean insert (T entity);
 }
