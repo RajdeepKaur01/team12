f8b8ae5802d35b8719e53b47b5db3b863263a195
sudeepk <sudeepk@ccs.neu.edu>
2017-03-25 16:53:28 -0400

    GetAuthorByName working

--- src/main/java/queryengine/AuthorDAO.java
+++ src/main/java/queryengine/AuthorDAO.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -14,14 +15,12 @@ public class AuthorDAO implements DAO<Author> {
 	
 	private static DAOFactory daoFactory = MariaDBDaoFactory.getInstance();
 	private static final Connection connection = daoFactory.getConnection();
+	
 	@Override
 	public Author findById(int id) throws SQLException {
 		PreparedStatement preparedStatement = connection.prepareStatement("select * from bibliography.author where ID = ?");
 		preparedStatement.setInt(1, id);
 		ResultSet resultSet = preparedStatement.executeQuery();
-		while(resultSet.next()) {
-			System.out.println(resultSet.getString(2));
-		}
 		return null;
 	}
 	
@@ -36,9 +35,17 @@ public class AuthorDAO implements DAO<Author> {
 	}
 
 	@Override
-	public List<Author> findByAttribute(String attirubteName, String attributeValue) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Author> findByAttribute(String attributeName, String attributeValue, int limit) throws SQLException {
+		PreparedStatement preparedStatement = connection.prepareStatement("select * from bibliography.author where " + attributeName + " = ? LIMIT " + limit);
+		preparedStatement.setString(1, attributeValue);
+		ResultSet resultSet = preparedStatement.executeQuery();
+		List<Author> authors = new ArrayList<>();
+		while (resultSet.next()) {
+			Author author = new Author();
+			author.setName(resultSet.getString(3));
+			System.out.println("Name is: " + author.getName());
+			authors.add(author);
+;		}
+		return authors;
 	}
-
 }
--- src/main/java/queryengine/DAO.java
+++ src/main/java/queryengine/DAO.java
@@ -7,6 +7,6 @@ import java.util.Map;
 public interface DAO <T>{
 	public T findById (int id) throws SQLException;
 	public List<T> findByAttributes (Map<String, String> attributeNamesAndValues);
-	public List<T> findByAttribute (String attirubteName, String attributeValue);
+	public List<T> findByAttribute (String attirubteName, String attributeValue, int limit) throws SQLException;
 	public boolean insert (T entity);
 }
