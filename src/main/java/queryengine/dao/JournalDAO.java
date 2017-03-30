4efec6ed5200fd48512c00daecd2a21131c515de
Mohit Gupta <gupta.moh@husky.neu.edu>
2017-03-30 18:49:33 -0400

    Team-12-26 update journal, handles search by year,added _key

--- src/main/java/queryengine/dao/JournalDAO.java
+++ src/main/java/queryengine/dao/JournalDAO.java
@@ -18,6 +18,7 @@ import main.java.queryengine.MariaDBDaoFactory;
 
 public class JournalDAO implements DAO<Journal> {
 	
+	private static final String YEAR="year";
 	private static DAOFactory daoFactory = MariaDBDaoFactory.getInstance();
 	private static final Connection connection = daoFactory.getConnection();
 	private String regex = "%";
@@ -27,13 +28,11 @@ public class JournalDAO implements DAO<Journal> {
 		PreparedStatement preparedStatement = connection.prepareStatement("select * from bibliography.journals where ID = ?");
 		preparedStatement.setInt(1, id);
 		ResultSet resultSet = preparedStatement.executeQuery();
-		List<Article> articles = new ArrayList<>();
 		Journal journal = new Journal();
 		while (resultSet.next()) {		
 			journal.setName(resultSet.getString(8));
 			journal.setVolume(resultSet.getString(5));
 			journal.setYear(resultSet.getInt(6));
-			ArticleDAO article = new ArticleDAO();
 			//TODO: DECIDE IF WE NEED IT
 		/*	Set<String> set = new HashSet<String>();
 			set.add(Integer.toString(resultSet.getInt(1)));
@@ -56,11 +55,17 @@ public class JournalDAO implements DAO<Journal> {
 		attributeValue.forEach((value) -> {
 			PreparedStatement preparedStatement;
 			try {
-				preparedStatement = connection.prepareStatement("select * from bibliography.journals where " + attributeName + " LIKE ? LIMIT " + limit);
+				if(attributeName.equals(YEAR)){
+					regex="";
+					preparedStatement = connection.prepareStatement("select * from bibliography.journals where " + attributeName + " = ? LIMIT " + limit);
+				} else{
+					preparedStatement = connection.prepareStatement("select * from bibliography.journals where " + attributeName + " LIKE ? LIMIT " + limit);
+				}
 				preparedStatement.setString(1, regex + value + regex);
 				ResultSet resultSet = preparedStatement.executeQuery();
 				while (resultSet.next()) {
 					Journal journal = new Journal();
+					journal.setKey(resultSet.getString(2));
 					journal.setYear(resultSet.getInt(6));
 					journal.setName(resultSet.getString(8));
 					journal.setVolume(resultSet.getString(5));
@@ -74,7 +79,6 @@ public class JournalDAO implements DAO<Journal> {
 					journals.add(journal);
 				}
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		});
@@ -84,18 +88,29 @@ public class JournalDAO implements DAO<Journal> {
 		
 		JournalDAO ob = new JournalDAO();
 		try {
+			System.out.println("Test 1- Search by ID");
 			Journal bo = ob.findById(1);
 			System.out.println(bo.getName());
 			System.out.println(bo.getVolume());
 			Set<String> set = new HashSet<String>();
 			set.add("Acta");
+			System.out.println("Test 2 Search by Attribute Journal");
 			List<Journal> bo2 = ob.findByAttribute("journal", set, 4);
 			for(Journal item : bo2){
 				System.out.println(item.getName());
 				System.out.println(item.getVolume());
 			}
+			System.out.println("Test 3 Search by Year ");
+			Set<String> set2 = new HashSet<String>();
+			set2.add("2001");
+			List<Journal> bo3 = ob.findByAttribute("year", set2, 4);
+			for(Journal item : bo3){
+				System.out.println(item.getKey());
+				System.out.println(item.getYear());
+				System.out.println(item.getName());
+				System.out.println(item.getVolume());
+			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally{
 			MariaDBDaoFactory.getInstance().closeConnection();
