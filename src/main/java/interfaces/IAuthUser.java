6c174e7a76cf371b999dc8e0e0554a98918eef0b
Mohit Gupta <gupta.moh@husky.neu.edu>
2017-04-11 19:56:45 -0400

    insert, select my-authors working #Team12-35

--- src/main/java/interfaces/IAuthUser.java
+++ src/main/java/interfaces/IAuthUser.java
@@ -1,13 +1,21 @@
 package main.java.interfaces;
 
-import java.util.List;
+import java.util.Set;
 
 import main.java.entities.Author;
+import main.java.entities.AuthorInfo;
+import main.java.entities.InProceeding;
+import main.java.entities.Journal;
+import main.java.entities.Proceedings;
+import main.java.queryengine.DAOFactory;
+import main.java.queryengine.MariaDBDaoFactory;
+import main.java.queryengine.dao.DAO;
 
 public interface IAuthUser {
-
-	public boolean addAuthors (int UserId,List<Author> authorList);
+	
+	
+	public boolean addAuthors (int UserId,Set<Author> authorList);
 	public boolean updateAuthors (int UserId, Author authorObj);
 	public boolean deleteAuthors(int UserId, Author authorObj);
-	public boolean getAuthors(int UserId);
+	public Set<Author> getAuthors(int UserId);
 }
